1bf826a96b33150914854d30df794a8c3de78b2e
RAFAELESPADA <pidgeypidgey1@gmail.com>
2025-12-27 17:37:26 -0300

    sk~çtg~]tttttttggTttttt~´iktttttttt

--- src/me/rafaelauler/sw/Automatic3.java
+++ src/me/rafaelauler/sw/Automatic3.java
@@ -440,7 +440,7 @@ org.bukkit.World w = Bukkit.getServer().getWorld(Main.cfg_x1.getString("x1.coord
 				    	oo.playSound(oo.getLocation(), Sound.valueOf("NOTE_PLING"), 10f, 10f);
 				    }
 
-				  Bukkit.broadcastMessage(ChatColor.GREEN + "Parabéns ao jogador " + firstPlayer.getName() + " por ganhar no mapa de skywars Antartica");
+				  Bukkit.broadcastMessage(ChatColor.GREEN + "Parabéns ao jogador " + firstPlayer.getName() + " por ganhar no mapa de skywars Marte");
 				  rodou = true;	
 				  new BukkitRunnable() {
 					  

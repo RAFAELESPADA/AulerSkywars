3bf374fd5b9bae08cd8a280d8b217e58b4141160
RAFAELESPADA <pidgeypidgey1@gmail.com>
2025-12-30 18:35:29 -0300

    byuttttttT45t

--- src/me/rafaelauler/sw/SkyWarsGame.java
+++ src/me/rafaelauler/sw/SkyWarsGame.java
@@ -427,7 +427,7 @@ started = true;
     }
 
     public void destroy() {
-        state = GameState.STARTING;
+        state = GameState.WAITING;
          
         for (UUID u : new ArrayList<>(players)) {
             Player p = Bukkit.getPlayer(u);
